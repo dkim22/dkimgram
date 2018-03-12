f6eff32f550c0a000a447aaca767db4cbacfc63d
dkim22 <ehddnr1186@gamil.com>
2018-03-13 00:51:40 +0900

    Initial setting

--- config/settings/base.py
+++ config/settings/base.py
@@ -39,7 +39,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db('DATABASE_URL', default='postgres://localhost/dkimgram'),
+    'default': env.db('DATABASE_URL', default='postgres://postgres:111111@localhost:5432/dkimgram'),
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
@@ -63,14 +63,13 @@ DJANGO_APPS = [
     'django.contrib.admin',
 ]
 THIRD_PARTY_APPS = [
-    'crispy_forms',
-
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
 ]
 LOCAL_APPS = [
     'dkimgram.users.apps.UsersConfig',
+    'dkimgram.images.apps.ImagesConfig',
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps

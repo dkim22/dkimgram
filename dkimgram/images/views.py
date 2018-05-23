44c43f2ca37e90839056de4a88453cdd71b5bd04
dkim22 <ehddnr1186@gamil.com>
2018-05-24 04:05:48 +0900

    Liking / Unliking Images

--- dkimgram/images/views.py
+++ dkimgram/images/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from . import models, serializers
-from django.shortcuts import get_object_or_404
 
 
 class Feed(APIView):
@@ -34,6 +34,29 @@ class LikeImage(APIView):
 
     def get(self, request, image_id, format=None):
 
-        print(image_id)
+        user = request.user
 
-        return Response(status=200)
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            preexsiting_like = models.Like.objects.get(
+                creator=user,
+                image=found_image
+            )
+            preexsiting_like.delete()
+
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        except models.Like.DoesNotExist:
+
+            new_like = models.Like.objects.create(
+                creator=user,
+                image=found_image
+            )
+
+            new_like.save()
+
+            return Response(status=status.HTTP_201_CREATED)

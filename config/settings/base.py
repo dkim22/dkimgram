110065a806ebd9ae1c80b9598bc9fab82788a635
dkim22 <ehddnr1186@gamil.com>
2018-07-05 21:25:34 +0900

    Created Notifications App and Model

--- config/settings/base.py
+++ config/settings/base.py
@@ -73,6 +73,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     'dkimgram.users.apps.UsersConfig',
     'dkimgram.images.apps.ImagesConfig',
+    'dkimgram.notifications.apps.NotificationsConfig'
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps

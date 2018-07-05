8e2fa2e2840097595cbb69b28afe517a344cbf79
dkim22 <ehddnr1186@gamil.com>
2018-07-06 02:39:24 +0900

    Editing a Photo, Deliting a Photo, Updating the Profile

--- dkimgram/images/models.py
+++ dkimgram/images/models.py
@@ -22,7 +22,7 @@ class Image(TimeStampedModel):
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
-    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True, related_name='images')
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True, related_name='images')
     tags = TaggableManager()
 
     @property
@@ -46,8 +46,8 @@ class Comment(TimeStampedModel):
     """ Comment Model """
 
     message = models.TextField()
-    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
-    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True, related_name='comments')
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True, related_name='comments')
 
     def __str__(self):
         return self.message
@@ -58,8 +58,8 @@ class Like(TimeStampedModel):
 
     """Like model """
 
-    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
-    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True, related_name='likes')
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True, related_name='likes')
 
     def __str__(self):
         return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
--- dkimgram/notifications/models.py
+++ dkimgram/notifications/models.py
@@ -13,10 +13,10 @@ class Notification(image_models.TimeStampedModel):
         ('follow', 'Follow')
     )
 
-    creator = models.ForeignKey(user_models.User, related_name='creator', on_delete=models.PROTECT)
-    to = models.ForeignKey(user_models.User, related_name='to', on_delete=models.PROTECT)
+    creator = models.ForeignKey(user_models.User, related_name='creator', on_delete=models.CASCADE)
+    to = models.ForeignKey(user_models.User, related_name='to', on_delete=models.CASCADE)
     notification_type = models.CharField(max_length=20, choices=TYPE_CHOICES)
-    image = models.ForeignKey(image_models.Image, null=True, blank=True, on_delete=models.PROTECT)
+    image = models.ForeignKey(image_models.Image, null=True, blank=True, on_delete=models.CASCADE)
     comment = models.TextField(null=True, blank=True)
 
     class Meta:

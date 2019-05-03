81880849a305af808124451dbdd7363e98a77f70
dkim22 <ehddnr1186@gamil.com>
2019-05-04 04:10:45 +0900

    Redux Version Change & Install react-router-dom

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createBrowserHistory } from "history";
+import { connectRouter, routerMiddleware } from "connected-react-router";
 import thunk from "redux-thunk";
 import users from "redux/modules/users";
 
 const env = process.env.NODE_ENV;
 
-const middlewares = [thunk];
+const history = createBrowserHistory();
+
+const middlewares = [thunk, routerMiddleware(history)];
 
 if (env === "development") {
   const { logger } = require("redux-logger");
@@ -12,9 +16,12 @@ if (env === "development") {
 }
 
 const reducer = combineReducers({
-  users
+  users,
+  router: connectRouter(history)
 });
 
 let store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+
+export { history };
 
 export default store();

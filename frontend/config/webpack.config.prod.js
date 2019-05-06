5292637cbdf409fbabcd04fca07875628ff96655
dkim22 <ehddnr1186@gamil.com>
2019-05-06 09:09:16 +0900

    Create Footer

--- frontend/config/webpack.config.prod.js
+++ frontend/config/webpack.config.prod.js
@@ -179,7 +179,8 @@ module.exports = {
                         minimize: true,
                         modules: true,
                         localIdentName: "[path][name]__[local]--[hash:base64:5]",
-                        sourceMap: shouldUseSourceMap
+                        sourceMap: shouldUseSourceMap,
+                        camelCase: "dashes"
                       }
                     },
                     {
